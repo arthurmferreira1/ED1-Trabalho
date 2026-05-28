35f20367382fb768ba7005e0e3e403c46a123814
DiegoMFLoureiro <diego.m.loureiro@edu.ufes.br>
2026-05-28 15:09:24 -0300

    Update main.c

--- main.c
+++ main.c
@@ -9,20 +9,17 @@ int main(){
     scanf("%d %d",&m,&n);
 
     mapa **mat = (mapa**)malloc(m * sizeof(mapa*));
-    if(mat==NULL){ printf("Erro de alocacao!\n"); exit(1); }
+    if(mat==NULL){ exit(1); }
     for(i=0;i<m;i++){
         mat[i] = (mapa*)malloc(n * sizeof(mapa));
-        if(mat[i]==NULL){ printf("Erro de alocacao!\n"); exit(1); }
+        if(mat[i]==NULL){ exit(1); }
     }
 
     for(i=0;i<m;i++){
         for(j=0;j<n;j++){
             mat[i][j].vis=0;
             do{
-                if(scanf("%c",&mat[i][j].simb)==0){
-                    printf("Tamanho Insuficiente!\n");
-                    exit(1);
-                }
+                scanf("%c",&mat[i][j].simb);
             }while(mat[i][j].simb!='.' && mat[i][j].simb!='#');
         }
     }
